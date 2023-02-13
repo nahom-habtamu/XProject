ccf86aeba3bc96ef4ef8ccc681feafa6e577bba7
Nahom Habtamu <nahomhab2626@gmail.com>
2023-02-13 17:58:11 +0300

    patch: add constructor function to the vehicle entity class   - rename DriverId prop to DriverIdentificationDocument

--- domain/vehicle/Vehicle.cs
+++ domain/vehicle/Vehicle.cs
@@ -5,22 +5,51 @@ namespace domain.vehicle
 {
     public class Vehicle
     {
-        public string? Id { get; set; }
-        public string? PlateNumber { get; set; }
-        public string? OwnerId { get; set; }
-        public string? City { get; set; }
-        public string? Type { get; set; }
-        public string? LoadType { get; set; }
+        public string Id { get; set; }
+        public string PlateNumber { get; set; }
+        public string OwnerId { get; set; }
+        public string City { get; set; }
+        public string Type { get; set; }
+        public string LoadType { get; set; }
         public DateTime ManufacturedDate { get; set; }
-        public string? Make { get; set; }
-        public string? Model { get; set; }
-        public string? LoadCapacity { get; set; }
-        public string? Color { get; set; }
-        public PersonId? DriverId { get; set; }
-        public Uri? CarImage { get; set; }
-        public Uri? LibreImage { get; set; }
-        public Uri? InsuranceImage { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string LoadCapacity { get; set; }
+        public string Color { get; set; }
+        public PersonId DriverIdentificationDocument { get; set; }
+        public Uri CarImage { get; set; }
+        public Uri LibreImage { get; set; }
+        public Uri InsuranceImage { get; set; }
         public DateTime LibreExpiryDate { get; set; }
         public DateTime InsuranceExpiryDate { get; set; }
+
+        public Vehicle(
+            string plateNumber, string ownerId, string city,
+            string type, string loadType, DateTime manufacturedDate,
+            string make, string model, string loadCapacity,
+            string color, PersonId driverIdentificationDocument, Uri carImage,
+            Uri libreImage, Uri insuranceImage, DateTime libreExpiryDate,
+            DateTime insuranceExpiryDate
+        )
+        {
+            Id = Guid.NewGuid().ToString("N");
+            PlateNumber = plateNumber;
+            OwnerId = ownerId;
+            City = city;
+            Type = type;
+            LoadType = loadType;
+            ManufacturedDate = manufacturedDate;
+            Make = make;
+            Model = model;
+            LoadCapacity = loadCapacity;
+            Color = color;
+            DriverIdentificationDocument = driverIdentificationDocument;
+            CarImage = carImage;
+            LibreImage = libreImage;
+            InsuranceImage = insuranceImage;
+            LibreExpiryDate = libreExpiryDate;
+            InsuranceExpiryDate = insuranceExpiryDate;
+        }
+
     }
 }
