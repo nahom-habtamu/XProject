092365a3cd912d0b3d5703c3638b6fed5046355e
Nahom Habtamu <nahomhab2626@gmail.com>
2023-02-20 10:54:59 +0300

    fix: rename GetCargoOwnersController to GetAllCargoOwnersController   - moved the file to separate namespace (cargoowners)

--- web/endpoints/cargoowner/GetAllCargoOwnersController.cs
+++ web/endpoints/cargoowner/GetAllCargoOwnersController.cs
@@ -3,20 +3,20 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace web.endpoints;
+namespace web.endpoints.cargoowners;
 
 [ApiController]
 [Route("[controller]")]
-public class GetCargoOwnersController : ControllerBase
+public class GetAllCargoOwnersController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<GetCargoOwnersController> _logger;
+    private readonly ILogger<GetAllCargoOwnersController> _logger;
 
-    public GetCargoOwnersController(ILogger<GetCargoOwnersController> logger)
+    public GetAllCargoOwnersController(ILogger<GetAllCargoOwnersController> logger)
     {
         _logger = logger;
     }
