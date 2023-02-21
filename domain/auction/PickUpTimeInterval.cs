b85341b5353d6173000692b0abad4df89090953e
Nahom Habtamu <nahomhab2626@gmail.com>
2023-02-21 13:36:34 +0300

    fix: change PickUpTimeInterval class constructor signature to accept strings rather than TimeSpan values

--- domain/auction/PickUpTimeInterval.cs
+++ domain/auction/PickUpTimeInterval.cs
@@ -5,9 +5,9 @@ public class PickUpTimeInterval
     public TimeSpan Min { get; set; }
     public TimeSpan Max { get; set; }
 
-    public PickUpTimeInterval(TimeSpan min, TimeSpan max)
+    public PickUpTimeInterval(string min, string max)
     {
-        Min = min;
-        Max = max;
+        Min = TimeSpan.Parse(min);
+        Max = TimeSpan.Parse(max);
     }
 }
