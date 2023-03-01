3fa2c6c85297d49b309b92730a2ea4777f159a95
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-01 10:28:16 +0300

    fix: change dummy implementation of  GetAllCargoOwnersController to real implementation of it

--- web/endpoints/cargoowner/GetAllCargoOwnersController.cs
+++ web/endpoints/cargoowner/GetAllCargoOwnersController.cs
@@ -1,26 +1,27 @@
+using domain.cargoowner;
 using Microsoft.AspNetCore.Mvc;
 
-namespace web.endpoints.cargoowners;
+namespace web.endpoints.cargoowner;
 
 [ApiController]
-[Route("[controller]")]
 public class GetAllCargoOwnersController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
+    private readonly CargoOwnerRepository _repository;
     private readonly ILogger<GetAllCargoOwnersController> _logger;
 
-    public GetAllCargoOwnersController(ILogger<GetAllCargoOwnersController> logger)
+    public GetAllCargoOwnersController(
+        ILogger<GetAllCargoOwnersController> logger,
+        CargoOwnerRepository repository
+    )
     {
         _logger = logger;
+        _repository = repository;
     }
 
-    [HttpGet(Name = "GetCargoOwners")]
-    public IEnumerable<string> Get()
+    [HttpGet]
+    [Route("[controller]")]
+    public async Task<List<CargoOwner>> Call()
     {
-        return Summaries.ToList();
+        return await _repository.GetAllCargoOwners();
     }
 }
