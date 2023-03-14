5b03fb1818f02f133e2cbacfe65849224a59fdc7
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-14 15:48:00 +0300

    patch: rename mispelled TradeLicense prop to correct spelling

--- dtos/cargoowner/CreateCargoOwnerRequestDto.cs
+++ dtos/cargoowner/CreateCargoOwnerRequestDto.cs
@@ -6,7 +6,7 @@ public class CreateCargoOwnerRequestDto
     public string? PhoneNumber { get; set; }
     public string? Email { get; set; }
     public string? SpecificAddress { get; set; }
-    public string? TradeLicence { get; set; }
+    public string? TradeLicense { get; set; }
     public string? PointPersonName { get; set; }
     public string? PointPersonPhoneNumber { get; set; }
     public string? PointPersonSpecificAddress { get; set; }
