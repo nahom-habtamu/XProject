1bba527e153f7fd122810cdf633b1d91d885096c
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-15 14:42:33 +0300

    refactor: use the DatabaseContext instead of constant values in AuctionRepositoryImpl

--- persistence/auction/AuctionRepositoryImpl.cs
+++ persistence/auction/AuctionRepositoryImpl.cs
@@ -1,11 +1,17 @@
 using Dapper;
-using Npgsql;
 using domain.auction;
 
 namespace persistence.auction;
 
 public class AuctionRepositoryImpl : AuctionRepository
 {
+    private readonly DatabaseContext _context;
+
+    public AuctionRepositoryImpl(DatabaseContext context)
+    {
+        _context = context;
+    }
+
     public Task<Auction?> Get(string id)
     {
         throw new NotImplementedException();
@@ -13,21 +19,19 @@ public class AuctionRepositoryImpl : AuctionRepository
 
     public async Task<List<Auction>> GetAllAuctions()
     {
-        using (var connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=ProjectX;User Id=postgres;Password=root"))
+        var connection = _context.Get();
+        var auctions = (await connection.QueryAsync<Auction, PriceInterval, PickUpTimeInterval, Auction>(
+        @"select id, cargoOwnerId, typeOfCargo, totalWeightOfCargo, deliveryplace, pickUpPlace, plannedPickUpDate, otherInformationAboutCargo,
+            minpriceperhundredkg as min, maxpriceperhundredkg as max, 
+            minpickuptime as min, minpickuptime as max from Auction
+        ", (auction, priceInterval, pickUpTimeInterval) =>
         {
-            var auctions = (await connection.QueryAsync<Auction, PriceInterval, PickUpTimeInterval, Auction>(
-            @"select id, cargoOwnerId, typeOfCargo, totalWeightOfCargo, deliveryplace, pickUpPlace, plannedPickUpDate, otherInformationAboutCargo,
-              minpriceperhundredkg as min, maxpriceperhundredkg as max, 
-              minpickuptime as min, minpickuptime as max from Auction
-            ", (auction, priceInterval, pickUpTimeInterval) =>
-            {
-                auction.PriceIntervalPerHundredKiloGram = priceInterval;
-                auction.PickUpTimeInterval = pickUpTimeInterval;
-                return auction;
-            }, splitOn: "id,min,min")).ToList();
+            auction.PriceIntervalPerHundredKiloGram = priceInterval;
+            auction.PickUpTimeInterval = pickUpTimeInterval;
+            return auction;
+        }, splitOn: "id,min,min")).ToList();
 
-            return auctions;
-        }
+        return auctions;
     }
 
     public Task<List<Auction>> GetAuctionsByCargoOwner(string id)
