68d140ddbf3f2f75948ead379d7f00c7eb057ad8
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-16 13:06:09 +0300

    refactor: tests inside CargoOwnerTest

--- project-x-test/cargoowner/CargoOwnerTest.cs
+++ project-x-test/cargoowner/CargoOwnerTest.cs
@@ -3,51 +3,37 @@ using domain.cargoowner;
 namespace project_x_test.cargoowner;
 public class CargoOwnerTest
 {
+    CargoOwner cargoOwnerOne = new CargoOwner(
+        "Nahom", "0926849888", "nahomhab2626@gmail.com", "Specific Address", "asasasasasasasa.jpg",
+        new CargoOwnerPointPerson("position", "abebe", "0911041221", "address", "abebe@gmail.com"),
+        "4650df78-9132-44d8-9168-4f90e31616e1"
+    );
+
     [Fact]
     public void ShouldReturnTrueWhenComparingCargoOwnerInstancesOfSameValue()
     {
-        var cargoOwnerOne = new CargoOwner(
-            "Nahom", "0926849888", "nahomhab2626@gmail.com", "Specific Address", "asasasasasasasa.jpg",
-            new CargoOwnerPointPerson("position", "abebe", "0911041221", "address", "abebe@gmail.com"),
-            "4650df78-9132-44d8-9168-4f90e31616e1"
-        );
-
         var cargoOwnerTwo = new CargoOwner(
             "Nahom", "0926849888", "nahomhab2626@gmail.com", "Specific Address", "asasasasasasasa.jpg",
             new CargoOwnerPointPerson("position", "abebe", "0911041221", "address", "abebe@gmail.com"),
             "4650df78-9132-44d8-9168-4f90e31616e1"
         );
-
-        Assert.Equal(cargoOwnerOne.Equals(cargoOwnerTwo), true);
-
+        Assert.Equal(cargoOwnerOne, cargoOwnerTwo);
     }
 
     [Fact]
     public void ShouldReturnFalseWhenComparingCargoOwnerInstancesOfDifferentPropertyValues()
     {
-        var cargoOwnerOne = new CargoOwner(
-            "Nahom", "0926849888", "nahomhab2626@gmail.com", "Specific Address", "asasasasasasasa.jpg",
-            new CargoOwnerPointPerson("position", "abebe", "0911041221", "address", "abebe@gmail.com"),
-            "4650df78-9132-44d8-9168-4f90e31616e1"
-        );
-
         var cargoOwnerTwo = new CargoOwner(
             "Nahom", "0926849888", "nahomhab2626@gmail.com", "Specific Address", "asasasasasasasa.jpg",
             new CargoOwnerPointPerson("position", "abebe", "0911041221", "address", "abebe@gmail.com"),
             "4650df78-9132-44d8-9168-4f90e31616e2"
         );
-
-        Assert.Equal(cargoOwnerOne.Equals(cargoOwnerTwo), false);
+        Assert.Equal(cargoOwnerOne, cargoOwnerTwo);
     }
 
     [Fact]
     public void ShouldReturnFalseWhenComparingWithNullValue()
     {
-        var cargoOwnerOne = new CargoOwner(
-            "NahomAbbee", "0926849888", "nahomhab2626@gmail.com", "Specific Address", "asasasasasasasa.jpg",
-            new CargoOwnerPointPerson("position", "abebe", "0911041221", "address", "abebe@gmail.com"),
-            "4650df78-9132-44d8-9168-4f90e31616e1"
-        );
         Assert.Equal(cargoOwnerOne.Equals(null), false);
     }
 }
