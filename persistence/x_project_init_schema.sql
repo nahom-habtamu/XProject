fca48fbe312f72a7687978d2d6d16577bd2990a1
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-20 10:44:47 +0300

    sql: add schema intiator and seeder files to persisience layer

---
+++ persistence/x_project_init_schema.sql
@@ -0,0 +1,31 @@
+create table if not exists CargoOwner(
+	id varchar(64) primary key,
+	name varchar(100) not null,
+	phoneNumber varchar(100) not null,
+	email varchar(100) not null,
+	specificAddress varchar(200) not null,
+	tradeLicense varchar(255) not null,
+	pointPersonName varchar(100) not null,
+	pointPersonPhoneNumber varchar(100) not null,
+	pointPersonSpecificAddress varchar(200) not null,
+	pointPersonEmail varchar(100) not null,
+	pointPersonPosition varchar(100) not null
+);
+
+create table if not exists Auction(
+	id varchar(64) primary key,
+	cargoOwnerId varchar(64) not null,
+	typeOfCargo varchar(15) not null,
+	totalWeightOfCargo int not null,
+	deliveryPlace varchar(100) not null,
+	pickUpPlace varchar(100) not null,
+	plannedPickUpDate date not null,
+	otherInformationAboutCargo varchar(255) not null,
+	minPricePerHundredKg float not null,
+	maxPricePerHundredKg float not null,
+	minPickUpTime varchar(5) not null,
+	maxPickUpTime varchar(5) not null,
+	CONSTRAINT cargoOwnerId
+      FOREIGN KEY(cargoOwnerId) 
+	  REFERENCES CargoOwner(id)
+);
---
+++ persistence/x_project_seed.sql
@@ -0,0 +1,44 @@
+/* Cargo Owner Seed */
+
+insert into cargoowner values(
+	'4650df78-9132-44d8-9168-4f90e31616e1','Cargo Owner 1','0926849881',
+	'cowner1@gmail.com','Shola, Addis Ababa','1250df78-9132-44d8-9168-4f90e31616e4.png',
+	'abebe gemechu','0911041221', 'Shola, Addis Ababa', 'abebe@gmail.com', 'NN'
+);
+
+insert into cargoowner values(
+	'4650df78-9132-44d8-9168-4f90e31616e2','Cargo Owner 2','0926849882',
+	'cowner2@gmail.com','Shola, Addis Ababa','0650df78-9132-44d8-9168-4f90e31616e4.png',
+	'abebe gemechu','0911041221', 'Shola, Addis Ababa', 'abebe@gmail.com', 'NN'
+);
+
+insert into cargoowner values(
+	'4650df78-9132-44d8-9168-4f90e31616e3','Cargo Owner 3','0926849883',
+	'cowner3@gmail.com','Shola, Addis Ababa','9650df78-9132-44d8-9168-4f90e31616e4.png',
+	'abebe gemechu','0911041221', 'Shola, Addis Ababa', 'abebe@gmail.com', 'NN'
+);
+
+/* Auction Seed */
+
+insert into auction values(
+	'1110df78-9132-44d8-9168-4f90e31616e1', '4650df78-9132-44d8-9168-4f90e31616e1',
+	'HEAVY',20,'Idk','Addis Ababa, Shola','02-03-2023',
+	'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+	135.45,180.90,'12:00','02:00'
+);
+
+insert into auction values(
+	'1110df78-9132-44d8-9168-4f90e31616e2', '4650df78-9132-44d8-9168-4f90e31616e2',
+	'LIGHT',20,'some place','Addis Ababa, Shola','03-04-2023',
+	'beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit',
+	35.45,43.90,'11:00','03:00'
+);
+
+insert into auction values(
+	'1110df78-9132-44d8-9168-4f90e31616e3', '4650df78-9132-44d8-9168-4f90e31616e3',
+	'MEDIUM',20,'someplace else','Addis Ababa, Shola','04-05-2023',
+	'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam vel illum qui dolorem eum fugiat',
+	55.45,73.90,'02:00','04:00'
+);
+
+
