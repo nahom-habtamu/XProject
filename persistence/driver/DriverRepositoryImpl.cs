52ec701b6d251553a9ab71ff1c0c7510e1506f3e
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-21 10:01:10 +0300

    feature: change mock impl of GetAllDrivers inside DriverRepositoryImpl to real database impl

--- persistence/driver/DriverRepositoryImpl.cs
+++ persistence/driver/DriverRepositoryImpl.cs
@@ -1,22 +1,32 @@
+using Dapper;
 using domain.driver;
 namespace persistence.driver;
 
 public class DriverRepositoryImpl : DriverRepository
 {
+    private readonly DatabaseContext _context;
+
+    public DriverRepositoryImpl(DatabaseContext context)
+    {
+        _context = context;
+    }
+
     public Task<Driver?> Get(string id)
     {
         throw new NotImplementedException();
     }
 
-    public Task<List<Driver>> GetAllDrivers()
+    public async Task<List<Driver>> GetAllDrivers()
     {
-        throw new NotImplementedException();
-
+        var connection = _context.Get();
+        var sql = @"SELECT id, name, phoneNumber, email, gender, 
+            dateOfBirth, specificAddress, drivingLicense FROM Driver";
+        var drivers = (await connection.QueryAsync<Driver>(sql)).ToList();
+        return drivers;
     }
 
     public Task Save(Driver entity)
     {
         throw new NotImplementedException();
-
     }
 }
