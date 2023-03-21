00773e8a94a37b23e22512a4b445fa1592186fc1
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-21 10:31:13 +0300

    refactor: extract out common baseGetSql variable and QueryAndParseResult method for making code more managable

--- persistence/driver/DriverRepositoryImpl.cs
+++ persistence/driver/DriverRepositoryImpl.cs
@@ -11,17 +11,26 @@ public class DriverRepositoryImpl : DriverRepository
         _context = context;
     }
 
-    public Task<Driver?> Get(string id)
+    private readonly string baseGetSql = @"SELECT id, name, phoneNumber, email, gender, 
+        dateOfBirth, specificAddress, drivingLicense FROM Driver";
+
+    public async Task<Driver?> Get(string id)
     {
-        throw new NotImplementedException();
+        var sql = baseGetSql + " WHERE id = " + "'" + id + "'";
+        var driver = (await QueryAndParseResult(sql)).FirstOrDefault();
+        return driver;
+    }
+
+    private async Task<IEnumerable<Driver>> QueryAndParseResult(string sql)
+    {
+        var connection = _context.Get();
+        var result = (await connection.QueryAsync<Driver>(sql));
+        return result;
     }
 
     public async Task<List<Driver>> GetAllDrivers()
     {
-        var connection = _context.Get();
-        var sql = @"SELECT id, name, phoneNumber, email, gender, 
-            dateOfBirth, specificAddress, drivingLicense FROM Driver";
-        var drivers = (await connection.QueryAsync<Driver>(sql)).ToList();
+        var drivers = (await QueryAndParseResult(baseGetSql)).ToList();
         return drivers;
     }
 
