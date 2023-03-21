cf3f58c953a093902020276264f1722e78786101
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-21 09:46:38 +0300

    test: add unit tests for Driver entity for checking equality operations

---
+++ project-x-test/driver/DriverTest.cs
@@ -0,0 +1,43 @@
+using domain.driver;
+
+namespace project_x_test.driver;
+public class DriverTest
+{
+    Driver driverOne = new Driver(
+        "7210df78-9132-44d8-9168-4f90e31616e1",
+        "Nahom Habtamu", "0926849888", "nahom@gmail.com", "0",
+        new DateTime(2023, 10, 9), "I dont know the address",
+        "1250df78-9132-44d8-9168-4f90e31616e4.png"
+    );
+
+    [Fact]
+    public void ShouldReturnTrueWhenComparingDriverInstancesOfSameValue()
+    {
+        Driver driverTwo = new Driver(
+            "7210df78-9132-44d8-9168-4f90e31616e1",
+            "Nahom Habtamu", "0926849888", "nahom@gmail.com", "0",
+            new DateTime(2023, 10, 9), "I dont know the address",
+            "1250df78-9132-44d8-9168-4f90e31616e4.png"
+        );
+        Assert.Equal(driverOne, driverTwo);
+    }
+
+    [Fact]
+    public void ShouldReturnFalseWhenComparingDriverInstancesOfDifferentPropertyValues()
+    {
+        Driver driverTwo = new Driver(
+            "7210df78-9132-44d8-9168-4f90e31616e2",
+            "Gagim Habtamu", "0926849888", "nahom@gmail.com", "0",
+            new DateTime(2023, 10, 9), "I dont know the address", 
+            "1250df78-9132-44d8-9168-4f90e31616e4.png"
+        );
+        
+        Assert.NotEqual(driverOne, driverTwo);
+    }
+
+    [Fact]
+    public void ShouldReturnFalseWhenComparingWithNullValue()
+    {
+        Assert.Equal(driverOne.Equals(null), false);
+    }
+}
