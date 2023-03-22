0db2e5546de9d9571976ae30100b0ec4499531ab
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-22 10:50:25 +0300

    test:  add one more assertion to make sure logic works inside GettingDriverWithIdThatExistShouldParseObjectSuccessfullyAndReturnDriver method of GetDriverControllerTest

--- project-x-test/driver/GetDriverControllerTest.cs
+++ project-x-test/driver/GetDriverControllerTest.cs
@@ -16,17 +16,27 @@ public class GetDriverControllerTest
     [Fact]
     public async Task GettingDriverWithIdThatExistShouldParseObjectSuccessfullyAndReturnDriver()
     {
-        var id = "7210df78-9132-44d8-9168-4f90e31616e1";
+        var idOne = "7210df78-9132-44d8-9168-4f90e31616e1";
+        var idTwo = "7210df78-9132-44d8-9168-4f90e31616e2";
         var sut = setUpSut();
 
-        var driverFound = await sut.Call(id);
-        Driver expectedDriverResult = new Driver(
+        var driverFoundForIdOne = await sut.Call(idOne);
+        var driverFoundForIdTwo = await sut.Call(idTwo);
+
+        Driver expectedDriverResultForIdOne = new Driver(
             "7210df78-9132-44d8-9168-4f90e31616e1",
             "Nahom Habtamu", "0926849888", "nahom@gmail.com", "0",
             new DateTime(2023, 4, 5), "Addis Ababa, Shola",
             "1250df78-9132-44d8-9168-4f90e31616e4.png"
         );
-        Assert.Equal(driverFound, expectedDriverResult);
+        Driver expectedDriverResultForIdTwo = new Driver(
+            "7210df78-9132-44d8-9168-4f90e31616e2",
+            "Dagim Habtamu", "0947977597", "dagim@gmail.com", "0",
+            new DateTime(2023, 4, 5), "Addis Ababa, Shola",
+            "8720df78-9132-44d8-9168-4f90e31616e4.png"
+        );
+        Assert.Equal(driverFoundForIdOne, expectedDriverResultForIdOne);
+        Assert.Equal(driverFoundForIdTwo, expectedDriverResultForIdTwo);
     }
 
     private GetDriverController setUpSut()
