e5c3f9e6265a25e5c4b2286a93fd5b918b630d5b
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-22 10:47:52 +0300

    test:  add one more assertion to make sure logic works inside GettingCargoOwnerWithIdThatExistShowParseObjectSuccessfullyAndReturnCargoOwner method of GetCargoOwnerControllerTest

--- project-x-test/cargoowner/GetCargoOwnerControllerTest.cs
+++ project-x-test/cargoowner/GetCargoOwnerControllerTest.cs
@@ -16,17 +16,29 @@ public class GetCargoOwnerControllerTest
     [Fact]
     public async Task GettingCargoOwnerWithIdThatExistShowParseObjectSuccessfullyAndReturnCargoOwner()
     {
-        var id = "4650df78-9132-44d8-9168-4f90e31616e1";
+        var idOne = "4650df78-9132-44d8-9168-4f90e31616e1";
+        var idTwo = "4650df78-9132-44d8-9168-4f90e31616e2";
         var sut = setUpSut();
 
-        var cargoOwnerFound = await sut.Call(id);
-        var expectedCargoOwnerResult = new CargoOwner(
+        var cargoOwnerFoundForIdOne = await sut.Call(idOne);
+        var cargoOwnerFoundForIdTwo = await sut.Call(idTwo);
+
+        var expectedCargoOwnerResultForIdOne = new CargoOwner(
             "4650df78-9132-44d8-9168-4f90e31616e1",
             "Cargo Owner 1", "0926849881", "cowner1@gmail.com",
             "Shola, Addis Ababa", "65650df78-9132-44d8-9168-4f90e31616e4.png",
             "abebe gemechu", "0911041221", "Shola, Addis Ababa", "abebe@gmail.com", "NN"
         );
-        Assert.Equal(cargoOwnerFound, expectedCargoOwnerResult);
+
+        var expectedCargoOwnerResultForIdTwo = new CargoOwner(
+            "4650df78-9132-44d8-9168-4f90e31616e2",
+            "Cargo Owner 2", "0926849882", "cowner2@gmail.com",
+            "Shola, Addis Ababa", "1230df78-9132-44d8-9168-4f90e31616e4.png",
+            "abebe gemechu", "0911041221", "Shola, Addis Ababa", "abebe@gmail.com", "NN"
+        );
+
+        Assert.Equal(cargoOwnerFoundForIdOne, expectedCargoOwnerResultForIdOne);
+        Assert.Equal(cargoOwnerFoundForIdTwo, expectedCargoOwnerResultForIdTwo);
     }
 
     private GetCargoOwnerController setUpSut()
