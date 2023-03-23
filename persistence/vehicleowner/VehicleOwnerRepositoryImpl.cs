d18601a6d131a83b2c7bb4ed52eba2da61e5b1c1
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-23 12:23:19 +0300

    feature: add real impl logic to the Get method of the VehicleOwnerRepositoryImpl  - change namespace  - adjust namespace change in the Program.cs file

--- persistence/vehicleowner/VehicleOwnerRepositoryImpl.cs
+++ persistence/vehicleowner/VehicleOwnerRepositoryImpl.cs
@@ -1,11 +1,22 @@
-using domain.common;
+using Dapper;
 using domain.vehicleowner;
 
+namespace persistence.vehicleowner;
 public class VehicleOwnerRepositoryImpl : VehicleOwnerRepository
 {
-    public Task<VehicleOwner?> Get(string id)
+    private readonly DatabaseContext _context;
+
+    public VehicleOwnerRepositoryImpl(DatabaseContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+
+    public async Task<VehicleOwner?> Get(string id)
+    {
+        var connection = _context.Get();
+        var sql = @"select id, name, phoneNumber, email, companyName, tradeLicense, userName, password from VehicleOwner WHERE id = " + "'" + id + "'";
+        var vehicleOwner = (await connection.QueryAsync<VehicleOwner>(sql)).FirstOrDefault();
+        return vehicleOwner;
     }
 
     public Task<List<VehicleOwner>> GetAllVehicleOwners()
