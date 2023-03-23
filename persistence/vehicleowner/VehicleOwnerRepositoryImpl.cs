f485098b66a7a73f5543dd20fc5463a96a708f26
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-23 10:55:34 +0300

    fix: remove dummy impl code from methods of  VehicleOwnerRepositoryImpl

--- persistence/vehicleowner/VehicleOwnerRepositoryImpl.cs
+++ persistence/vehicleowner/VehicleOwnerRepositoryImpl.cs
@@ -3,13 +3,6 @@ using domain.vehicleowner;
 
 public class VehicleOwnerRepositoryImpl : VehicleOwnerRepository
 {
-    List<VehicleOwner> vehicleOwners = new List<VehicleOwner> {
-        new VehicleOwner("Nahom", new MobileNumber("0926849888"),"nahom@gmail.com", "Abebe PLC", new Uri("https://learn.microsoft.com/en-us/aspnet/core/mvc/controllers/routing?view=aspnetcore-7.0"), "naHom","password"),
-        new VehicleOwner("Nahom1", new MobileNumber("0926849888"),"nahom@gmail1.com", "Abebe PLC1", new Uri("https://learn.microsoft.com/en-us/aspnet/core/mvc/controllers/routing?view=aspnetcore-7.0"), "naHom1","password1"),
-        new VehicleOwner("Nahom2", new MobileNumber("0926849888"),"nahom@gmail2.com", "Abebe PLC2", new Uri("https://learn.microsoft.com/en-us/aspnet/core/mvc/controllers/routing?view=aspnetcore-7.0"), "naHom2","password2"),
-        new VehicleOwner("Nahom3", new MobileNumber("0926849888"),"nahom@gmail3.com", "Abebe PLC3", new Uri("https://learn.microsoft.com/en-us/aspnet/core/mvc/controllers/routing?view=aspnetcore-7.0"), "naHom3","password3"),
-    };
-
     public Task<VehicleOwner?> Get(string id)
     {
         throw new NotImplementedException();
@@ -17,7 +10,7 @@ public class VehicleOwnerRepositoryImpl : VehicleOwnerRepository
 
     public Task<List<VehicleOwner>> GetAllVehicleOwners()
     {
-        return Task.Run(() => vehicleOwners.ToList());
+        throw new NotImplementedException();
     }
 
     public Task Save(VehicleOwner entity)
