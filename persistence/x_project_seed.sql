83fb5d1c830f0c48b21fafb07ab734293f197c2f
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-23 12:40:25 +0300

    sql: change vehicle owner seeder code to format the tradeLicense prop as a json list

--- persistence/x_project_seed.sql
+++ persistence/x_project_seed.sql
@@ -72,10 +72,10 @@ insert into VehicleOwner values(
 	'2150df78-9132-44d8-9168-4f90e31616e1',
 	'Vehicle Owner 1', '0926849881', 'vo1@gmail.com',
 	'VO1 Company', 
- 	'[
-	   ''2150df78-9132-44d8-9168-4f90e31616e1.jpg'',
-	   ''2150df78-9132-44d8-9168-4f90e31616e1.jpg''
-  	]',
+ 	$$[
+	   "2150df78-9132-44d8-9168-4f90e31616e1.jpg",
+	   "2150df78-9132-44d8-9168-4f90e31616e1.jpg"
+  	]$$,
 	'vehicleowner-1',
 	'vehicleowner1pass'
 );
@@ -84,10 +84,10 @@ insert into VehicleOwner values(
 	'2150df78-9132-44d8-9168-4f90e31616e2',
 	'Vehicle Owner 2', '0926849882', 'vo2@gmail.com',
 	'VO2 Company', 
- 	'[
-	   ''2150df78-9132-44d8-9168-4f90e31616e2.jpg'',
-	   ''2150df78-9132-44d8-9168-4f90e31616e2.jpg''
-  	]',
+ 	$$[
+	   "2150df78-9132-44d8-9168-4f90e31616e2.jpg",
+	   "2150df78-9132-44d8-9168-4f90e31616e2.jpg"
+  	]$$,
 	'vehicleowner-2',
 	'vehicleowner2pass'
 );
@@ -96,10 +96,10 @@ insert into VehicleOwner values(
 	'2150df78-9132-44d8-9168-4f90e31616e3',
 	'Vehicle Owner 3', '0926849883', 'vo3@gmail.com',
 	'VO3 Company', 
- 	'[
-	   ''2150df78-9132-44d8-9168-4f90e31616e3.jpg'',
-	   ''2150df78-9132-44d8-9168-4f90e31616e3.jpg''
-  	]',
+ 	$$[
+	   "2150df78-9132-44d8-9168-4f90e31616e3.jpg",
+	   "2150df78-9132-44d8-9168-4f90e31616e3.jpg"
+  	]$$,
 	'vehicleowner-3',
 	'vehicleowner3pass'
 );
