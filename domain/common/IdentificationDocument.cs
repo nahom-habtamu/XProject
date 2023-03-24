1e596b773d5fd4b5780b7a25f09837fe80ff03ed
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-24 10:03:20 +0300

    feature: add equality checker method to the IdentificationDocument entity

--- domain/common/IdentificationDocument.cs
+++ domain/common/IdentificationDocument.cs
@@ -2,7 +2,7 @@ namespace domain.common;
 public class IdentificationDocument
 {
     public List<string> Value { get; }
-    
+
     public IdentificationDocument(List<string> value)
     {
         if (value.Count == 2)
@@ -13,5 +13,37 @@ public class IdentificationDocument
         {
             throw new InvalidDataException();
         }
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (ReferenceEquals(obj, this))
+            return false;
+
+        if (obj.GetType() != this.GetType())
+            return false;
+
+        var parsed = obj as IdentificationDocument;
+
+
+        var x = this.Value[0].Equals(parsed!.Value[0]);
+        var y = this.Value[1].Equals(parsed!.Value[1]);
+
+        if (
+            this.Value[0].Equals(parsed!.Value[0]) &&
+            this.Value[1].Equals(parsed!.Value[1]))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return this.GetHashCode();
     }
 }
