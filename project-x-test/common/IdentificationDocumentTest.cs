c5f7504ddda4f16a0c5f2955b67a04824be2ffb0
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-24 10:04:16 +0300

    test: add more unit tests to IdentificationDocument entity for verifying equal method

--- project-x-test/common/IdentificationDocumentTest.cs
+++ project-x-test/common/IdentificationDocumentTest.cs
@@ -1,7 +1,7 @@
 using domain.common;
 
 namespace project_x_test.common;
-public class PersonIdTest
+public class IdentificationDocumentTest
 {
     [Fact]
     public void ThrowsInvalidDataExceptionWhenPassedListIsNotEqualToTwoInlength()
@@ -29,5 +29,32 @@ public class PersonIdTest
 
         var id = new IdentificationDocument(listOfImagesWithTwoValues);
         Assert.Equal(id.Value.Count, listOfImagesWithTwoValues.Count);
+    }
+
+    [Fact]
+    public void ShouldReturnTrueWhenComparingIdentificationDocumentInstancesOfSameValue()
+    {
+        var document = new List<string> { "11d2dd123320olk000l.jpg", "11d2dd123320olk0002.jpg", };
+        Assert.Equal(
+            new IdentificationDocument(document).Equals(new IdentificationDocument(document)
+        ), true);
+    }
+
+    [Fact]
+    public void ShouldReturnFalseWhenComparingIdentificationDocumentInstancesOfDifferentPropertyValues()
+    {
+        var documentOne = new List<string> { "11d2dd123320olk000l.jpg", "11d2dd123320olk0002.jpg", };
+        var documentTwo = new List<string> { "11d2dd123320olk0003.jpg", "11d2dd123320olk0004.jpg", };
+
+        Assert.Equal(
+            new IdentificationDocument(documentOne).Equals(new IdentificationDocument(documentTwo)
+        ), false);
+    }
+
+    [Fact]
+    public void ShouldReturnFalseWhenComparingWithNullValue()
+    {
+        var document = new List<string> { "11d2dd123320olk000l.jpg", "11d2dd123320olk0002.jpg", };
+        Assert.Equal(new IdentificationDocument(document).Equals(null), false);
     }
 }
