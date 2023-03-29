ba671568a0fa36030ec15bad733c828a18da4ba8
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-29 09:31:36 +0300

    fix: remove dummy impl code from the BidRepositoryImpl and left it unimplemented

--- persistence/bid/BidRepositoryImpl.cs
+++ persistence/bid/BidRepositoryImpl.cs
@@ -4,29 +4,21 @@ namespace persistence.bid;
 
 public class BidRepositoryImpl : BidRepository
 {
-    private List<Bid> bids = new List<Bid>
-    {
-        new Bid("1","1",2000,"None"),
-        new Bid("2","1",2000,"None"),
-        new Bid("3","1",2000,"None"),
-        new Bid("1","2",2000,"None"),
-        new Bid("5","7",2000,"None"),
-        new Bid("6","3",2000,"None"),
-    };
 
     public Task<Bid?> Get(string id)
     {
-        return Task.Run(() => bids.FirstOrDefault(b => b.Id.Equals(id)));
+        throw new NotImplementedException();
     }
 
     public Task<List<Bid>> GetBidsByDriver(string id)
     {
-        return Task.Run(() => bids.Where(b => b.DriverId.Equals(id)).ToList());
+        throw new NotImplementedException();
     }
 
     public Task<List<Bid>> GetBidsForAuction(string id)
     {
-        return Task.Run(() => bids.Where(b => b.AuctionId.Equals(id)).ToList());
+        throw new NotImplementedException();
+
     }
 
     public Task Save(Bid entity)
