8f7138ea07e0c865c89581e41f1f2dadd2d88f7b
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-31 09:56:02 +0300

    fix: remove Ilogger from the CreateDriverController

--- web/endpoints/driver/CreateDriverController.cs
+++ web/endpoints/driver/CreateDriverController.cs
@@ -7,15 +7,12 @@ namespace web.endpoints.driver;
 [ApiController]
 public class CreateDriverController : ControllerBase
 {
-    private readonly ILogger<CreateDriverController> _logger;
     private readonly DriverRepository _repository;
 
     public CreateDriverController(
-        ILogger<CreateDriverController> logger,
         DriverRepository repository
     )
     {
-        _logger = logger;
         _repository = repository;
     }
 
