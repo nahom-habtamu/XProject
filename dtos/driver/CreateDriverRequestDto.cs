f12d15f01b2ee8a92b7e18e83e6247359f10e6f2
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-31 12:30:07 +0300

    feature: add Id prop

--- dtos/driver/CreateDriverRequestDto.cs
+++ dtos/driver/CreateDriverRequestDto.cs
@@ -2,6 +2,7 @@ namespace dtos.driver;
 
 public class CreateDriverRequestDto
 {
+    public string? Id { get; set; }
     public string? Name { get; set; }
     public string? PhoneNumber { get; set; }
     public string? Email { get; set; }
